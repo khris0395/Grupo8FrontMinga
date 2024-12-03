0a43ccc4578bbf35de9f3dfa26a4980686513462
Juan Carlos Forero <juan.forero66@gmail.com>
2024-12-03 08:43:02 -0500

    se crea chapterForm se actualiza el footer para que en vista mobile no se vea y se actuliza el home para vista mobile y al manga form se le agrega mensaje para cuado sea exitosa la creacion del manga

--- src/store/reducer/chapterRecuer.js
+++ src/store/reducer/chapterRecuer.js
@@ -1,26 +1,43 @@
-// import { createReducer } from "@reduxjs/toolkit"
-// import { fetchChapter } from "../actions/mangaActions"
+import { createReducer } from "@reduxjs/toolkit"
+import { fetchChapter, createChapter } from "../actions/chapterActions" // Actualizado el import
 
-// const initialState = {
-//     chapter: [],
-//     loading: false,
-//     error: null,
-// }
+const initialState = {
+    chapter: [],
+    loading: false,
+    error: null,
+    createSuccess: false // Agregado para manejar el éxito de la creación
+}
 
-// export const chapterReducer = createReducer(initialState, (builder) => {
-//     builder
-//         .addCase(fetchChapter.pending, (state) => {
-//             state.loading = true;
-//             state.error = null;
-//         })
-//         .addCase(fetchChapter.fulfilled, (state, action) => {
-//             state.selectedManga = action.payload;
-//             state.loading = false;
-//             state.error = null;
-//         })
-//         .addCase(fetchChapter.rejected, (state, action) => {
-//             state.loading = false;
-//             state.error = action.error.message;
-//         })
-
-// })
+export const chapterReducer = createReducer(initialState, (builder) => {
+    builder
+        .addCase(fetchChapter.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchChapter.fulfilled, (state, action) => {
+            state.selectedManga = action.payload;
+            state.loading = false;
+            state.error = null;
+        })
+        .addCase(fetchChapter.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
+        // Agregados los casos para createChapter
+        .addCase(createChapter.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+            state.createSuccess = false;
+        })
+        .addCase(createChapter.fulfilled, (state, action) => {
+            state.chapter.push(action.payload);
+            state.loading = false;
+            state.error = null;
+            state.createSuccess = true;
+        })
+        .addCase(createChapter.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+            state.createSuccess = false;
+        })
+})
