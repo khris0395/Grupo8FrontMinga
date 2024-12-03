d3a5042dc3b6328880c9f382cc799233182a6c6f
Juan Carlos Forero <juan.forero66@gmail.com>
2024-12-02 20:37:17 -0500

    se implementa edithChapter

--- store/store.js
+++ store/store.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit"
 import { mangaReducer } from "./reducer/mangaRecuer.js"
 import authReducer from "./reducer/authReducer.js"
 import { categoryReducer } from './reducer/categoryReducer.js'
-
+import { EditChapter } from "./reducer/edithChapterReducer.js"
 
 const store = configureStore({
      reducer:{
         mangas: mangaReducer,
         authStore: authReducer,
-        categories: categoryReducer
+        categories: categoryReducer,
+        editChapter: EditChapter
      }
  })
 export default store 
