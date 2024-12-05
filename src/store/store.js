dbc8ec3f86e515692f5dfe9327fa0a2061825c56
jheesfri <jheesfryc@gmail.com>
2024-12-04 22:02:30 -0500

    solucion de error en pagina MangaForm

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { mangaReducer } from "./reducer/mangaRecuer.js"
-
+import { configureStore } from "@reduxjs/toolkit";
+import { mangaReducer } from "./reducer/mangaRecuer";
+import authReducer from "./reducer/authReducer";
+import { categoryReducer } from './reducer/categoryReducer';
+import { EditChapter } from "./reducer/edithChapterReducer";
+import { authorReducer } from "./reducer/authorReducer";
+import { adminPanelReducer } from "./reducer/adminPanelReducer";
+import { companyReducer } from "./reducer/companyReducer";
 
 const store = configureStore({
-     reducer:{
-        mangas: mangaReducer
-     }
- })
-export default store 
+  reducer: {
+    mangas: mangaReducer,
+    authStore: authReducer,
+    categories: categoryReducer,
+    EditChapter: EditChapter,
+    author: authorReducer,
+    adminPanel: adminPanelReducer,
+    company: companyReducer
+  }
+});
+
+export default store;
