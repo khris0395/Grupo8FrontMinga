6e85b223d2a56303d8e38a3b1dfba246a5ae7497
Carlos Fernandez <krlosafernandez@gmail.com>
2024-12-06 22:22:37 -0500

    cree pagina AuthorProfile

--- src/pages/EditAuthor.jsx
+++ src/pages/EditAuthor.jsx
@@ -1,14 +1,14 @@
-import MangaCard from "../components/MangaCard";
-
 import EdithAuthor from "../components/EditAuthor/EdithAuthor copy 2";
+import AuthorProfile from "../components/AuthorProfile/AuthorProfile";
+import UserProfile from "../components/AuthorProfile/Ap";
 
 export default function EditAuthor() {
     return (
         <>
-      
-            
+             <AuthorProfile />
+             
+            {/* <EdithAuthor /> */}
 
-            <EdithAuthor />
         </>
     );
 }
