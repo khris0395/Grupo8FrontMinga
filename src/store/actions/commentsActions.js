a6d09d13893ca0683df06a868d4a12e9655e3d96
Juan Carlos Forero <juan.forero66@gmail.com>
2024-12-08 10:12:23 -0500

    comentarios a medias

--- src/store/actions/commentsActions.js
+++ src/store/actions/commentsActions.js
@@ -5,8 +5,15 @@ import axios from "axios";
 export const fetchComment = createAsyncThunk(
     "author/fetchComment",
     async (commentId) => {
-        const response = await axios.get(`http://localhost:8080/api/commentById/${commentId}`);
-        console.log("fetch comment",response.data.response);
+        const response = await axios.get(`http://localhost:8080/api/commentById/${commentId}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+
+        console.log("fetch comment", response.data.response);
         return response.data.response
     }
 );
@@ -15,7 +22,37 @@ export const fetchComment = createAsyncThunk(
 export const fetchCommentAC = createAsyncThunk(
     "comment/fetchCommentAC",
     async () => {
-        const response = await axios.get('http://localhost:8080/comments', { params: filter });
+        const response = await axios.get('http://localhost:8080/api/comments/allComments',
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
         console.log(response.data);
-    } 
+    }
+);
+
+
+export const createComment = createAsyncThunk(
+    "comment/createComment",
+    async ({ commentData, token, chapterId }) => {
+        try {
+            const response = await axios.post(
+                `http://localhost:8080/api/comments/createComment`,
+                {
+                    ...commentData,
+                    chapter_id: chapterId
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+            return response.data.response;
+        } catch (error) {
+            throw error;
+        }
+    }
 );
